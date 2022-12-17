76fdcb48a1984263d94069d79450c3d95cbc24d4
keithgaines <93451842+keithgaines@users.noreply.github.com>
2022-12-16 22:46:54 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT | 3000
+process.chdir(__dirname);
 
 app.use(bodyParser.urlencoded({extended: false})); app.use(express.static(path.join(__dirname, 'public')));
 
