301b8448b6094dd6fb64c6756d0adcf03d8a77c4
keithgaines <93451842+keithgaines@users.noreply.github.com>
2022-12-16 21:11:15 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const path = require('path');
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({extended: false})); app.use(express.static(path.join(__dirname, 'public')));
+
+const server = http.createServer(app);
+server.listen(port);
+
+app.get('/', express.static(path.join(__dirname, "/public")));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
